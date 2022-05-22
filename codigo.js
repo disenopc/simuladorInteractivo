041bd097ed9b28d5072446a0fb0d607afb2da0ae
disenopc <capitanellipl@gmail.com>
2022-05-22 19:23:54 -0300

    mejore los metodos de pago

--- codigo.js
+++ codigo.js
@@ -19,7 +19,7 @@ function compraPetshop(mascota) {
 
 const mascotaRubro = function(mascota, rubro) { return mascota + rubro }
 let mascota = prompt("¿Que animal tenes de mascota?");
-while (mascota != "ESC") {
+while (mascota != '') {
     let rubro = parseInt(prompt("Que estas buscando? \n 1-Alimento balanceado \n 2-Accesorios \n (Ingrese el número correspondiente)"));
     console.log(mascotaRubro(mascota, rubro));
     if (rubro == 1) {
@@ -34,43 +34,50 @@ while (mascota != "ESC") {
     let cantidad = "";
     const precioW = 150;
     const precioJ = 130;
-    const waltcan = (kgW) => kgW * precioW;
-    const jaspe = (kgJ) => kgJ * precioJ;
+    let descuentoEfectivo = 0.10;
+    let descuentoDebito = 0.03;
+
     if ((rubro = "balanceado") || (rubro = "Balanceado") || (rubro = "alimento balanceado") || (rubro = "ALIMENTO BALANCEADO")) {
         let bolsa = prompt("Ingrese la marca de alimento de su preferencia \nWaltcan \nJaspe");
         let cantidad = prompt("De cuantos kg \n 15kg \n 25kg");
-        switch (bolsa) {
-            case "waltcan":
-                alert(waltcan(cantidad));
-                break;
-            case "jaspe":
-                alert(jaspe(cantidad));
-                break;
 
+        let formaDePago = parseInt(prompt("Ingrese con que metodo va a abonar\n 1-Efectivo \n 2-Tarjeta de credito \n 3-Tarjeta de debito \n (Ingrese el numero correspondiente"));
+        if (bolsa = "waltcan") {
+            if (formaDePago == 1) {
+                let precioEfectivoW = (precioW * cantidad) - (descuentoEfectivo * precioW * cantidad);
+                alert("El precio de tu alimento es: $" + precioEfectivoW);
+                break;
+            } else if (formaDePago == 3) {
+                let precioDebitoW = (precioW * cantidad) - (descuentoDebito * precioW * cantidad);
+                alert("El precio de tu alimento es: $" + precioDebitoW);
+                break;
+            }
+            alert("El precio de tu alimento es: $" + precioW);
+            break;
         }
-    } else
-    if ((rubro = "accesorios") || (rubro = "Accesorios") || (rubro = "ACCESORIOS")) {
+        if (bolsa = "jaspe") {
+            if (formaDePago == 1) {
+                let precioEfectivoJ = (precioJ * cantidad) - (descuentoEfectivo * precioJ * cantidad);
+                alert("El precio de tu alimento es: $" + precioEfectivoJ);
+                break;
+            } else if (formaDePago == 2) {
+                let precioDebitoJ = (precioJ * cantidad) - (descuentoDebito * precioJ * cantidad);
+                alert("El precio de tu alimento es: $" + precioDebitoJ);
+                break;
+            }
+            alert("El precio de tu alimento es: $" + precioJ);
+            break;
+        }
+
+    } else if ((rubro = "accesorios") || (rubro = "Accesorios") || (rubro = "ACCESORIOS")) {
         let tipoDeAccesorio = prompt("Ingrese el accesorio de su preferencia");
         let cantidad = parseInt(prompt("Ingrese la cantidad"));
         alert("Su eleccion es: " + tipoDeAccesorio + "Cantidad: " + cantidad + "kg.");
     }
 
-    const formaDePago = prompt("Ingrese con que metodo va a abonar\n 1-Efectivo \n 2-Tarjeta de credito \n 3-Tarjeta de debito");
-    let precioEfectivo = precio - (precio * 0.10);
-    let precioDebito = precio - (precio * 0.03);
-    switch (formaDePago) {
-        case "efectivo":
-            alert("El precio final de su compra es " + precioEfectivo);
-            break;
-        case "tarjeta de credito":
-            alert("El precio final de su compra es " + precio);
-            break;
-        case "tarjeta de debito":
-            alert("El precio final de su compra es " + precioDebito);
-            break;
-        default:
-            break;
-    }
+
+
+
 }
 alert("Corresponde ingresar tipo de mascota");
 console.log(mascota + rubro + bolsa + cantidad);
