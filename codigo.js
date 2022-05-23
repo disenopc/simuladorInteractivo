9d0b5bf53fd604646b5646954d60de9ca88148bb
disenopc <capitanellipl@gmail.com>
2022-05-23 19:36:07 -0300

    html y funcion

--- codigo.js
+++ codigo.js
@@ -15,17 +15,11 @@
 
 
 const mascotaRubro = function(mascota, rubro) { return mascota + rubro }
-let mascota = prompt("¿Que animal tenes de mascota?");
-while (mascota != '') {
-    let rubro = parseInt(prompt("Que estas buscando? \n 1-Alimento balanceado \n 2-Accesorios \n (Ingrese el número correspondiente)"));
+let mascota = prompt("¿Cómo se llama tu mascota?");
+//Seleccionar alimento balanceado o accesorio
+while (mascota != "") {
+    let rubro = parseInt(prompt("Qué estás buscando para " + mascota + "?" + "\n 1 - Alimento balanceado \n 2 - Accesorios \n(Ingrese el número correspondiente)"));
     console.log(mascotaRubro(mascota, rubro));
-    if (rubro == 1) {
-        alert("Mascota: " + mascota + ", " + "busca: Alimento Balanceado");
-    } else if (rubro == 2) {
-        alert("Mascota: " + mascota + ", " + "busca: Accesorios");
-    }
-    //Seleccionar alimento balanceado o accesorio
-
     const mascotaAlimento = function(bolsa, cantidad) { return bolsa + cantidad }
     let bolsa = "";
     let cantidad = "";
@@ -33,12 +27,11 @@ while (mascota != '') {
     const precioJ = 130;
     let descuentoEfectivo = 0.10;
     let descuentoDebito = 0.03;
-
-    if ((rubro = "balanceado") || (rubro = "Balanceado") || (rubro = "alimento balanceado") || (rubro = "ALIMENTO BALANCEADO")) {
+    if (rubro == "1") {
+        alert("Mascota: " + mascota + ", " + "busca: Alimento Balanceado");
         let bolsa = prompt("Ingrese la marca de alimento de su preferencia \nWaltcan \nJaspe");
         let cantidad = prompt("De cuantos kg \n 15kg \n 25kg");
-
-        let formaDePago = parseInt(prompt("Ingrese con que metodo va a abonar\n 1-Efectivo \n 2-Tarjeta de credito \n 3-Tarjeta de debito \n (Ingrese el numero correspondiente"));
+        let formaDePago = parseInt(prompt("Ingrese con que metodo va a abonar\n 1-Efectivo \n 2-Tarjeta de credito \n 3-Tarjeta de debito \n (Ingrese el numero correspondiente)"));
         if (bolsa = "waltcan") {
             if (formaDePago == 1) {
                 let precioEfectivoW = (precioW * cantidad) - (descuentoEfectivo * precioW * cantidad);
@@ -49,7 +42,7 @@ while (mascota != '') {
                 alert("El precio de tu alimento es: $" + precioDebitoW);
                 break;
             }
-            alert("El precio de tu alimento es: $" + precioW);
+            alert("El precio de tu alimento es: $" + precioW * cantidad);
             break;
         }
         if (bolsa = "jaspe") {
@@ -62,22 +55,17 @@ while (mascota != '') {
                 alert("El precio de tu alimento es: $" + precioDebitoJ);
                 break;
             }
-            alert("El precio de tu alimento es: $" + precioJ);
+            alert("El precio de tu alimento es: $" + precioJ * cantidad);
             break;
         }
-
-    } else if ((rubro = "accesorios") || (rubro = "Accesorios") || (rubro = "ACCESORIOS")) {
+    } else if (rubro == 2) {
+        alert("Mascota: " + mascota + ", " + "busca: Accesorios");
         let tipoDeAccesorio = prompt("Ingrese el accesorio de su preferencia");
         let cantidad = parseInt(prompt("Ingrese la cantidad"));
-        alert("Su eleccion es: " + tipoDeAccesorio + "Cantidad: " + cantidad + "kg.");
+        alert("Su eleccion es: " + tipoDeAccesorio + " Cantidad: " + cantidad + " .");
     }
-
-
-
-
 }
+
+console.log(mascota + rubro + bolsa + cantidad);
 alert("Corresponde ingresar tipo de mascota");
-console.log(mascota + rubro + bolsa + cantidad);
-//RECORDATORIO: AL FINAL HACCER EL BUCLE PARA QUE CUANDO PONGA OK SE CALCULE EL PRECIO + DESCUENTO EN EFECTIVO
-console.log(mascota + rubro + bolsa + cantidad);
 //RECORDATORIO: AL FINAL HACCER EL BUCLE PARA QUE CUANDO PONGA OK SE CALCULE EL PRECIO + DESCUENTO EN EFECTIVO
