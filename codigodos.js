4d837d349e8a7792378a99bbcf041941167fd659
disenopc <capitanellipl@gmail.com>
2022-05-23 22:34:56 -0300

    js

--- codigodos.js
+++ codigodos.js
@@ -4,6 +4,7 @@ let precioUnitario;
 let cantidad;
 let precioFinal;
 let resultado;
+let descuentoEfectivo;
 
 function formaDePago() {
     cantidad = parseInt(prompt('De cuantos kg prefiere la bolsa de Alimento Balanceado \n 15kg \n 25kg \n(Solo ingresar el número'));
@@ -31,6 +32,18 @@ function operar(num1, num2, precioFinal) {
             break;
     }
 }
+//funcion flecha
+let efectivo = (a, b) => {
+    console.log(a - b);
+    return a - b;
+};
+
+const resta = (a, b) => a - b
+const descuentoEf = 0.1 * resultado;
+
+function descuentoEfectivof() {
+    descuentoEfectivo = resta(resultado, descuentoEf);
+}
 
 
 //FUNCION DOS
@@ -40,7 +53,7 @@ function operar(num1, num2, precioFinal) {
 function descuento() {
     if (precioFinal == "si") {
         alert("Se le realizara un descuento por pago en efectivo del 10%")
-        return true;
+        return descuentoEf;
     } else {
         alert("Se le cobrara precio de lista");
         return false;
@@ -48,7 +61,7 @@ function descuento() {
 }
 
 formaDePago();
-while (!descuento()) {}
-
 resultado = operar(precioUnitario, cantidad, precioFinal);
 mostrar("El precio final de tu producto es $" + resultado);
+descuento();
+mostrar("El precio final de tu producto es $" + descuentoEfectivo);
